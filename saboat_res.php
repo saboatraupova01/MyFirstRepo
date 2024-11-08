ea2b7d15e2a54da6fc57fb9d2fdf074655f1701b
Saboat <saboatraupova01@gmail.com>
2024-11-08 14:18:08 +0300

    Hello

---
+++ saboat_res.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Резюме</title>
+</head>
+<body>
+    <header>
+        <h1>Раупова Сабоат</h1>
+        <p>+992-92-700-64-32</p>
+    </header>
+    <section id="objective">
+        <h2>Дата рождения:</h2>
+        <p>6 августа 2007 гг.</p>
+    </section>
+    <section id="experience">
+        <h2>Электронная почта:</h2>
+        <ul>
+            <li>
+                <p>saboatraupova01@gmail.com</p>
+                
+            </li>
+        </ul>
+    </section>
+    <section id="education">
+        <h2>Образование</h2>
+        <ul>
+            <li>
+                <h3>Физико-математический лицей №1 имени академика М.Осими</h3>
+                <p>11 класс</p>
+            </li>
+        </ul>
+    </section>
+  
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+header {
+    background-color:lightseagreen;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+h1, h2, h3 {
+    margin: 0;
+    padding: 0;
+}
+
+section {
+    background: #fff;
+    margin: 20px;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+ul li {
+    margin-bottom: 10px;
+}
+#experience h3 {
+    color: #333;
+    margin-bottom: 5px;
+}
+
+#experience p {
+    margin-left: 20px;
+    color: #666;
+}
+@media print {
+    body {
+        background: #fff;
+        color: #000;
+    }
+
+    section {
+        box-shadow: none;
+        margin: 0;
+        padding: 0;
+    }
+}
+    </style>
+</body>
+</html>
